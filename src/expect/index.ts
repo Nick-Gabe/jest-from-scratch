6659f3dac9df413d8a762c6b7becf34b64a75556
nick-gabe <nickygamer023@gmail.com>
2022-11-27 16:59:41 -0300

    first commit

---
+++ src/expect/index.ts
@@ -0,0 +1,25 @@
+class Expect {
+  private value: unknown;
+
+  constructor(value: unknown) {
+    this.value = value;
+  }
+
+  toBe(comparison: unknown) {
+    return Object.is(this.value, comparison)
+  }
+
+  toEqual(comparison: unknown) {
+    return this.value == comparison
+  }
+
+  toStrictlyEqual(comparison: unknown) {
+    return this.value === comparison
+  }
+}
+
+export const expect = (value: unknown) => {
+  if(typeof value === 'function') value = value();
+
+  return new Expect(value)
+}
